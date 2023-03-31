1c532362068824fef6078d0c3bdb64f3806464e1
tien.nguyen <tien.nguyen@epandcompany.com>
2023-03-31 11:05:33 -0500

    update package-lock

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,11 +9,6 @@ import { createStatusUpdates } from '../graphql/mutations';
 import { listStatusUpdates } from '../graphql/queries';
 
 export function Profile () {
-    // const { route } = useAuthenticator((context) => [context.route]);
-    //
-    // const message =
-    //     route === 'authenticated' ? 'FIRST PROTECTED ROUTE!' : 'Loading...';
-    // return <Heading level={1}>{message}</Heading>;
 
     const { route } = useAuthenticator((context) => [context.route]);
     const navigate = useNavigate();
